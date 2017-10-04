02c568dfa90148459133ff9465a77b8ac4084c0a
kikimiriti <32511125+kikimiriti@users.noreply.github.com>
2017-10-04 15:09:57 +0300

    Add files via upload

---
+++ Hello-there.py
@@ -0,0 +1,4 @@
+username= input ("what is your name?\n")
+username= input ("what is your age?\n")
+username= input ("where is your area of locality?\n")
+print ("hello immah, aged 25 from Kiambu County,am Kiki; pleasure to meet you") 
